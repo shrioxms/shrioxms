b1b3fc278d084fc9a9c1f511a904e327973cbb4a
shrioxms <info@shrioxms.com>
2026-01-25 22:34:22 +0530

    added my id

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: Request) {
     }
 
     await resend.emails.send({
-      from: "Contact Form <hr@shrioxms.com>",
+      from: "Contact Form <admin@shrioxms.com>",
       // from: "Contact Form <onboarding@resend.dev>",
       to: [process.env.RECEIVER_EMAIL!],
       subject: `New Enquiry from ${name}`,
