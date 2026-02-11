eebe024f4389b7750c029766e3c83ccc42f7b7ee
shrioxms <info@shrioxms.com>
2026-02-11 16:12:24 +0530

    noreply_infogmail

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -20,8 +20,8 @@ export async function POST(req: Request) {
     }
 
     await resend.emails.send({
-      // from: "Contact Form <noreply@shrioxms.com>",
-      from: "Contact Form <onboarding@resend.dev>",
+      from: "Contact Form <noreply@shrioxms.com>",
+      // from: "Contact Form <onboarding@resend.dev>",
       to: [process.env.RECEIVER_EMAIL!],
       subject: `New Enquiry from ${name}`,
       html: `
